ff7b61aa56c491c87cf30ff7f5e8f0a3f056a622
ACH1LL35 <zobayeralam1025@gmail.com>
2023-11-12 17:20:30 +0600

    href update

--- UserComplaint.php
+++ UserComplaint.php
@@ -212,8 +212,8 @@
             <li><a href="UserVenueBook.php">BOOK VENUE</a></li>
             <li><a href="UserVenueHistory.php">BOOKING HISTORY</a></li>
             <li><a href="#">UPCOMING</a></li>
-            <li><a href="#">Query</a></li>
-            <li><a href="#">Ask for Quotation</a></li>
+            <li><a href="UserQuery.php">Query</a></li>
+            <li><a href="UserQuotation.php">Ask for Quotation</a></li>
             <li><a href="UserComplaint.php">Feedback</a></li>
         </ul>
     </div>
