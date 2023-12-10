f9a7a62b71ef21b6f4fb3644332231e3be2b6b02
ACH1LL35 <zobayeralam1025@gmail.com>
2023-12-10 23:12:48 +0600

    phpmailer

--- Signup.php
+++ Signup.php
@@ -1,4 +1,8 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 $successMessage = "";
 $errorMessages = [];
 
@@ -43,7 +47,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $sql = "INSERT INTO credential (name, email, cnumber, password) VALUES ('$name', '$email', '$cnumber', '$password')"; // Updated SQL query
 
         if ($mysqli->query($sql) === true) {
-            $successMessage = "Registration successful!";
+            // Send email using PHPMailer
+            require 'vendor/autoload.php';
+            require("PHPMailer/PHPMailer.php");
+            require("PHPMailer/SMTP.php");
+            require("PHPMailer/Exception.php");
+
+            $mail = new PHPMailer();
+
+            try {
+                //Server settings
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'eventa2zmanagement@gmail.com';                     //SMTP username
+                $mail->Password   = 'kawn bptd orqf nmci';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            
+                //Recipients
+                $mail->setFrom('eventa2zmanagement@gmail.com', 'A2Z EVENTS');
+                $mail->addAddress($email);     //Add a recipient
+    
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Email verification from A2Z Events';
+                $mail->Body    = "Thanks for registration! 
+                click the link below to verify the mail address 
+                <a href='http://localhost/project/verify.php?email=$email&verifi_code=$verifi_code'> Verify </a>";
+            
+                $mail->send();
+
+                $successMessage = "Registration successful! Verification email sent.";
+            } catch (Exception $e) {
+                $errorMessages[] = "Error sending verification email: " . $mail->ErrorInfo;
+            }
         } else {
             $errorMessages[] = "Error: " . $sql . "<br>" . $mysqli->error;
         }
@@ -109,7 +147,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     </script>
 
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup Page</title>
     <style>
---
+++ Update_password.php
@@ -0,0 +1,177 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "event_management";
+$con = new mysqli($servername, $username, $pass, $dbname);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+if(isset($_GET['email']) && isset($_GET['reset_token']))
+{
+    date_default_timezone_set('Asia/Dhaka');
+    $date = date("Y-m-d");
+    $query = "SELECT * FROM `credential` WHERE `email`=? AND `resettoken`=?";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "ss", $_GET['email'], $_GET['reset_token']);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if($result)
+    {
+        if(mysqli_num_rows($result) == 1)
+        {
+            $row = mysqli_fetch_assoc($result);
+            
+            if ($row['resettoken'] !== null) {
+                echo "
+                    <div id='divarea'>
+                        <form method='POST'>
+                            <h1>Create New Password</h1>
+                            <input id='pass' type='password' name='password' placeholder='New Password' required>
+                            <input id='con_pass' type='password' name='con_password' placeholder='Confirm Password' required>
+                            <button id='update_btn' name='update_btn'> Update </button>
+                        </form>
+                    </div>
+                ";
+            } else {
+                echo "
+                    <script>
+                        alert('Password has already been updated.');
+                        window.location.href='#';
+                    </script>
+                ";
+            }
+        }
+        else
+        {
+            echo "
+                <script>
+                    alert('Invalid or Expired Link');
+                    window.location.href='#';
+                </script>
+            ";
+        }
+    }
+    else
+    {
+        echo "
+            <script>
+                alert('Server Down! try again later');
+                window.location.href='#';
+            </script>
+        ";
+    }
+}
+
+?>
+
+<?php
+
+if(isset($_POST['update_btn']))
+{
+    $password = $_POST['password'];
+    $confirm_password = $_POST['con_password'];
+
+    if ($password == $confirm_password) {
+        $update_pass = password_hash($password, PASSWORD_BCRYPT);
+        $update_query = "UPDATE `credential` SET `password`=?, `resettoken`=NULL WHERE `email`=?";
+        $stmt = mysqli_prepare($con, $update_query);
+        mysqli_stmt_bind_param($stmt, "ss", $update_pass, $_GET['email']);
+
+        if(mysqli_stmt_execute($stmt))
+        {
+            echo "
+                <script>
+                    alert('Password Updated Successfully');
+                    window.location.href='#';
+                </script>
+            ";
+            exit; // Add this line to prevent further execution
+        }
+        else
+        {
+            echo "
+                <script>
+                    alert('Server Down! try again later');
+                    window.location.href='#';
+                </script>
+            ";
+        }
+    } else {
+        echo "
+            <script>
+                alert('Password and Confirm Password do not match');
+                window.location.href='#';
+            </script>
+        ";
+    }
+}
+
+?>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100%;
+  width: 100%;
+  background-color: #f0f2f5;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  position: static;
+}
+
+h1 {
+    margin-bottom: 50px;
+}
+
+#divarea {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    background: #fff;
+    box-shadow: 0 0 10px #ddd;
+    border-radius: 10px;
+    padding: 15px;
+    text-align: center;
+}
+
+#pass, #con_pass {
+    text-align: center;
+    width: 100%;
+    display: block;
+    border: 1px solid #ddd;
+    padding: 14px 16px;
+    font-size: 19px;
+    border-radius: 6px;
+    margin-bottom: 15px;
+}
+
+#update_btn {
+    text-align: center;
+    width: 85%;
+    display: block;
+    border: 1px solid #ddd;
+    padding: 14px 16px;
+    font-size: 19px;
+    border-radius: 6px;
+    margin-bottom: 15px;
+    margin-inline: 7.5%;
+    background-color: coral;
+    font-weight: bolder;
+}
+
+#update_btn:hover {
+    background-color: rgb(154, 49, 11);
+}
+</style>
