96de46323d45da6daa28a7bf83987792c0a23b84
ACH1LL35 <zobayeralam1025@gmail.com>
2023-12-14 00:25:59 +0600

    cleaning

--- AdminEventHistory.php
+++ AdminEventHistory.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include('AdminSidebar.php');
 
 if (isset($_POST['logout'])) {
--- start.php
+++ start.php
@@ -1,6 +1,3 @@
 <?php
-// You may include other necessary files or configurations here.
-
-// Include the controller to start the application.
 include('controller/admin_login_controller.php');
 ?>
